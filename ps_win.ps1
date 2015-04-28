e10a3f29a6810943bc79065a0639a93daeb0f8e3
zhchu <zhchu@cisco.com>
2015-04-28 14:20:00 +0800

    refine for windows

--- ps_win.ps1
+++ ps_win.ps1
@@ -1,5 +1,6 @@
 
-function set_env($name, $value) {
+function set_env($name, $value) 
+{
     if ($value -eq "") {
         return
     }
@@ -8,7 +9,8 @@ function set_env($name, $value) {
     }
 }
 
-function set_path($value) {
+function set_path($value) 
+{
     if ($value -eq "") {
         return
     }
@@ -18,7 +20,8 @@ function set_path($value) {
     }
 }
 
-function refresh_env {   
+function refresh_env 
+{
     $locations = 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Environment','HKCU:\Environment'
     $locations | ForEach-Object {   
         $k = Get-Item $_
@@ -30,7 +33,8 @@ function refresh_env {
     }
 }
 
-function send_msg {
+function send_msg 
+{
     if (-not ("Win32.NativeMethods" -as [Type])) {
         # import sendmessagetimeout from win32
         Add-Type -Namespace Win32 -Name NativeMethods -MemberDefinition @"
@@ -48,46 +52,62 @@ function send_msg {
    [Win32.Nativemethods]::SendMessageTimeout($HWND_BROADCAST, $WM_SETTINGCHANGE, [UIntPtr]::Zero, "Environment", $flags, 5000, [ref] $result);
 }
 
-function check_install($bin, $pkg, $opt, $path) {
-    $ret = "False"
-    if ($bin -ne "") {
+function check_install([string]$bin, $pkg, $opt, [string]$ver, $path) 
+{
+    $ret = False
+    if ($bin.Length -ne 0) {
         cmd /C where $bin
         $ret = $?
     }
-    if ($ret -eq "False") {
+    if (!$ret) {
         echo "[WARN] To install $pkg ..."
-        cinst $pkg -y $opt
+        if ($ver.Length -ne 0) {
+            cinst $pkg -y $opt -version="$ver"
+        }else {
+            cinst $pkg -y $opt
+        }
         set_path "$path"
     }
+}
+
+function pip_install($pkg)
+{
+    pip list $pkg | grep "^$pkg "
+    pip install $pkg
 }
 
 
 
 ##======================================
 
-function check_env {
+function check_env 
+{
     if ($env:Home -ne $env:UserProfile) {
         set_env "HOME" "$env:UserProfile"
     }
 }
 
-function check_base {
-    check_install -bin whois -pkg sysinternals -path "C:\Tools\sysinternals"
+function check_base 
+{
+    #check_install -bin whois -pkg sysinternals -path "C:\Tools\sysinternals"
     check_install -bin 7z -pkg 7zip -opt "-f" -path "C:\Program Files\7-Zip"
     check_install -bin cmake -pkg cmake
     check_install -pkg freeSSHD
 }
 
-function check_dev {
+function check_dev 
+{
     $ant_home = "C:\Tools\apache-ant-1.9.4"
-    check_install -bin ant -pkg ant -opt "-f" -path "$ant_home\bin"
+    check_install -bin ant -pkg ant -opt "-f" -ver "1.9.4" -path "$ant_home\bin"
     if ($env:ANT_HOME -ne $ant_home) {
         set_env "ANT_HOME" "$ant_home"
     }
     
     cmd /C where git
-    if ($? -eq "False") {
-        cinst git --params="/GitAndUnixToolsOnPath /NoAutoCrlf" -y -f
+    $ret = $?
+    if (!$ret) {
+        cinst git.install --params="/GitAndUnixToolsOnPath /NoAutoCrlf" -y
+        set_path "C:\Program Files (x86)\Git\bin"
     }
 
     check_install -bin cygwin -pkg cygwin -opt "-f" -path "C:\Tools\cygwin\bin"
@@ -98,18 +118,21 @@ function check_dev {
 function check_python
 {
     check_install -bin python -pkg python2 -opt "-f"
-    pip install python-dateutil
-    pip install numpy
-    pip install matplotlib docutils sphinx treelib
-    pip install pymatlab scipy
-}
+    cmd /C where pip
+    $ret = $?
+    if (!$ret) {
+        echo "Cannot find pip for python ..."
+        return
+    }
 
-function check_ruby
-{
-    check_install -bin ruby -pkg ruby -opt "-f"
-    check_install -pkg rubygems
-    check_install -bin -pkg ruby2.devkit -opt "-f"
-    check_install -bin node -pkg nodejs.install -opt "-f"
+    pip_install -pkg python-dateutil
+    #pip_install -pkg numpy
+    #pip_install -pkg matplotlib 
+    #pip_install -pkg pymatlab
+    #pip_install -pkg scipy
+    pip_install -pkg docutils 
+    pip_install -pkg sphinx 
+    pip_install -pkg treelib 
 }
 
 function check_android
@@ -118,21 +141,77 @@ function check_android
     #cinst android-ndk -y --source=http://dl.google.com/android/ndk/android-ndk-r10d-windows-x86_64.exe
 }
 
+function check_ruby
+{
+    check_install -bin ruby -pkg ruby -opt "-f" -ver "2.1.5" -path "C:\Tools\ruby215\bin"
+    check_install -pkg rubygems
+
+    check_install -bin devkitvars -pkg ruby2.devkit -path "C:\tools\DevKit2"
+    set_path "C:\tools\DevKit2\bin"
+    
+    Push-Location
+    cd "C:\tools\DevKit2"
+    ruby dk.rb init
+    ruby dk.rb install
+    Pop-Location
+
+    check_install -bin node -pkg nodejs.install -opt "-f"
+}
+
 function check_calabash
 {
+    #gem sources -r https://rubygems.org/
+    #gem sources -a http://rubygems.org/
+    echo "=> $root\update\.gemrc, $env:HOME\.gemrc"
+    Copy-Item $root\update\.gemrc $env:HOME\.gemrc -Force
+
     gem update --system 2.3.0
-    gem install erubis
 
-    gem uninstall cucumber --force
-    gem install cucumber -v 1.3.18
+    gem list erubis -i 
+    $ret = $?
+    if (!$ret) {
+        gem install erubis
+    }
+    
+    gem list cucumber -i -v 1.3.18
+    $ret = $?
+    if (!$ret) {
+        gem uninstall cucumber --force
+        gem install cucumber -v 1.3.18
+    }
 
-    rvm use default
-    rvm gemset create calabash
-    rvm gemset use calabash
 
-    #gem list | grep "^calabash " 2>/dev/null || gem install calabash
-    #gem list | grep "^calabash-android " 2>/dev/null || gem install calabash-android
+    cmd /C where rvm
+    $ret = $?
+    if (!$ret) {
+        Push-Location
+        cd $root\update
+        #curl -sSL https://get.rvm.io > rvm.sh 
+        #C:\Tools\cygwin\bin\bash rvm.sh -s stable --ruby
+        Pop-Location
+
+        #rvm use default
+        #rvm gemset create calabash
+        #rvm gemset use calabash
+    }
+
+
+
+    gem list -l | grep "^calabash " 
+    $ret = $?
+    if (!$ret) {
+        gem install calabash
+    }
+
+    gem list -l | grep "^calabash-android " 
+    $ret = $?
+    if (!$ret) {
+        gem install calabash-android
+    }
 }
+
+
+
 
 #########################
 # prepare installed tools
@@ -142,8 +221,14 @@ function check_calabash
 #   android ndk
 #########################
 
-check_env
-check_base
-check_dev
+
+$root = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+#check_env
+#check_base
+#check_dev
 #check_python
-#check_ruby
+check_ruby
+check_calabash
+
+
