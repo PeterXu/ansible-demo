5d85ab1ba5ae6850850feb6e303bd3cb0150040c
Yongze Xu <test@test>
2015-05-13 16:06:45 +0800

    add py vc

--- ps_set.ps1
+++ ps_set.ps1
@@ -1,4 +1,5 @@
 # set privillege
+$env_home = "$env:UserProfile"
 
 $rd = Get-Random
 $fprc = "test_$rd.txt"
@@ -15,5 +16,23 @@ if (Test-Path "$choco") {
 $tools = "C:\Tools"
 if (Test-Path "$tools") {
     Set-Acl $tools $acl
+}
+
+
+$client = New-Object System.Net.WebClient
+$url = "http://download.microsoft.com/download/7/9/6/796EF2E4-801B-4FC4-AB28-B59FBF6D907B/VCForPython27.msi"
+$dest = "$env_home\VCForPython27.msi"
+Try {
+    if (Test-Path $dest) {
+    }else {
+        $client.DownloadFile($url, $dest)
+    }
+
+    if (Test-Path $dest) {
+        $logfile = [IO.Path]::GetTempFileName()
+        $extra_args = ""
+        msiexec.exe /i $dest /qb /l $logfile $extra_args;
+    }
+}Catch {
 }
 
