5c4ce9206f923718ecf077e2d5ce50ce8eef7883
Yongze Xu <test@test>
2015-05-15 13:30:10 +0800

    add kit tools

--- ps_win.ps1
+++ ps_win.ps1
@@ -190,6 +190,7 @@ function check_env
         set_env "HOME" "$env_home"
     }
     set_path "$choco_home\bin"
+    set_path "C:\Program Files (x86)\Windows Kits\8.1\Tools\x64"
     reload_env
 }
 
